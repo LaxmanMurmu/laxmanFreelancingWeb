4428bc3abe7e8ce88cba1e2fe661f0681df551e0
Laxman Murmu <laxmanmurmu8201@gmail.com>
2023-06-21 19:08:16 +0530

    woked on serve page

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -1,6 +1,7 @@
 <x-_header />
 <x-_navbar />
 <style>
+    
       .head_background_image {
         background-image: url("{{ Storage::url('images/a-trending-plant-species-in-cyberspace-with-the-te-2UDCC69.jpg') }}");
         background-color: #000000;
@@ -27,6 +28,17 @@
     border-radius: 35px;
     cursor: pointer;
 }
+.back_image_con {
+        background-image: url("{{ Storage::url('images/thisisengineering-raeng-sbFmoKBK7jU-unsplash.jpg') }}");
+        background-color: #000000;
+        height: 90vh;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        filter: brightness(0.7);
+        position: fixed;
+        z-index: -1;
+    }
 </style>
 
 <div class="bg-light">
@@ -34,10 +46,13 @@
 
 
   <!-- ============================================================Heading background============================ -->
-  <div class="head_background_image container-fluid d-flex justify-content-center align-items-center">
+  <!-- <div class="head_background_image container-fluid d-flex justify-content-center align-items-center">
         <h1 style="font-family: 'Archivo Black', sans-serif; font-size: 60px; font-weight: bold;" class="text-center">Projects</h1>
-    </div>
+    </div> -->
 
+
+    <div class="back_image_con container-fluid">
+    </div>
 
 
 
--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -28,9 +28,26 @@
         border-radius: 35px;
         cursor: pointer;
     }
-</style>
 
-<div class="bg-white">
+    .back_image_con {
+        background-image: url("{{ Storage::url('images/thisisengineering-raeng-sbFmoKBK7jU-unsplash.jpg') }}");
+        background-color: #000000;
+        height: 100vh;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        filter: brightness(0.7);
+        position: fixed;
+        z-index: -1;
+        top: 0px;
+    }
+</style>
+<div class="back_image_con container-fluid">
+</div>
+<div class="bg-transparent">
+
+
+
 
 
 
@@ -42,96 +59,242 @@
 
     <!-- ============================================================Servive Topics============================ -->
 
-    <div class="service_row1_con my-5 container">
-        <div class="row">
+    <div class="bg-white">
+        <div class="service_row1_con py-5 container">
+            <div class="row">
 
-            <div class=" col-md-4" style="height: 350px;">
-                <div class="col-md-12 text-center bg-black text-light p-5" style="border: 22px solid white;">
-                    <i class="fa-solid fa-paintbrush mb-2" style="font-size: 30px;"></i>
-                    <h3 class="my-2">Design</h3>
-                    <p class="text_cut">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis dignissimos lorem Lorem ipsum... Kamal </p>
-                    <h6 class="my-3 more_box">
-                        <p class="More_text">More ↓</p>
-                        <p class="myText">laxman doloribus iste error, eligendi maiores itaque ab voluptas consectetur
-                            debitis </p>
-                    </h6>
+                <div class=" col-md-4" style="height: 350px;">
+                    <div class="col-md-12 text-center bg-black text-light p-5" style="border: 22px solid white;">
+                        <i class="fa-solid fa-paintbrush mb-2" style="font-size: 30px;"></i>
+                        <h3 class="my-2">Design</h3>
+                        <p class="text_cut">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis
+                            dignissimos lorem Lorem ipsum... Kamal </p>
+                        <h6 class="my-3 more_box">
+                            <p class="More_text">More ↓</p>
+                            <p class="myText">laxman doloribus iste error, eligendi maiores itaque ab voluptas
+                                consectetur
+                                debitis </p>
+                        </h6>
+                    </div>
                 </div>
-            </div>
 
-           
-            <div class=" col-md-4" style="height: 350px;">
-                <div class="col-md-12 text-center bg-black text-light p-5" style="border: 22px solid white;">
-                    <i class="fa-solid fa-paintbrush mb-2" style="font-size: 30px;"></i>
-                    <h3 class="my-2">Design</h3>
-                    <p class="text_cut">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis dignissimos lorem Lorem ipsum... Kamal </p>
-                    <h6 class="my-3 more_box">
-                        <p class="More_text">More ↓</p>
-                        <p class="myText">laxman doloribus iste error, eligendi maiores itaque ab voluptas consectetur
-                            debitis </p>
-                    </h6>
+
+                <div class=" col-md-4" style="height: 350px;">
+                    <div class="col-md-12 text-center bg-black text-light p-5" style="border: 22px solid white;">
+                        <i class="fa-solid fa-paintbrush mb-2" style="font-size: 30px;"></i>
+                        <h3 class="my-2">Design</h3>
+                        <p class="text_cut">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis
+                            dignissimos lorem Lorem ipsum... Kamal </p>
+                        <h6 class="my-3 more_box">
+                            <p class="More_text">More ↓</p>
+                            <p class="myText">laxman doloribus iste error, eligendi maiores itaque ab voluptas
+                                consectetur
+                                debitis </p>
+                        </h6>
+                    </div>
                 </div>
-            </div>
 
-            <div class=" col-md-4" style="height: 350px;">
-                <div class="col-md-12 text-center bg-black text-light p-5" style="border: 22px solid white;">
-                    <i class="fa-solid fa-paintbrush mb-2" style="font-size: 30px;"></i>
-                    <h3 class="my-2">Design</h3>
-                    <p class="text_cut">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis dignissimos lorem Lorem ipsum... Kamal </p>
-                    <h6 class="my-3 more_box">
-                        <p class="More_text">More ↓</p>
-                        <p class="myText">laxman doloribus iste error, eligendi maiores itaque ab voluptas consectetur
-                            debitis </p>
-                    </h6>
+                <div class=" col-md-4" style="height: 350px;">
+                    <div class="col-md-12 text-center bg-black text-light p-5" style="border: 22px solid white;">
+                        <i class="fa-solid fa-paintbrush mb-2" style="font-size: 30px;"></i>
+                        <h3 class="my-2">Design</h3>
+                        <p class="text_cut">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis
+                            dignissimos lorem Lorem ipsum... Kamal </p>
+                        <h6 class="my-3 more_box">
+                            <p class="More_text">More ↓</p>
+                            <p class="myText">laxman doloribus iste error, eligendi maiores itaque ab voluptas
+                                consectetur
+                                debitis </p>
+                        </h6>
+                    </div>
                 </div>
-            </div>
 
 
-            <div class=" col-md-4" style="height: 350px;">
-                <div class="col-md-12 text-center bg-black text-light p-5" style="border: 22px solid white;">
-                    <i class="fa-solid fa-paintbrush mb-2" style="font-size: 30px;"></i>
-                    <h3 class="my-2">Design</h3>
-                    <p class="text_cut">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis dignissimos lorem Lorem ipsum... Kamal </p>
-                    <h6 class="my-3 more_box">
-                        <p class="More_text">More ↓</p>
-                        <p class="myText">laxman doloribus iste error, eligendi maiores itaque ab voluptas consectetur
-                            debitis </p>
-                    </h6>
+                <div class=" col-md-4" style="height: 350px;">
+                    <div class="col-md-12 text-center bg-black text-light p-5" style="border: 22px solid white;">
+                        <i class="fa-solid fa-paintbrush mb-2" style="font-size: 30px;"></i>
+                        <h3 class="my-2">Design</h3>
+                        <p class="text_cut">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis
+                            dignissimos lorem Lorem ipsum... Kamal </p>
+                        <h6 class="my-3 more_box">
+                            <p class="More_text">More ↓</p>
+                            <p class="myText">laxman doloribus iste error, eligendi maiores itaque ab voluptas
+                                consectetur
+                                debitis </p>
+                        </h6>
+                    </div>
                 </div>
-            </div>
 
 
-            <div class=" col-md-4" style="height: 350px;">
-                <div class="col-md-12 text-center bg-black text-light p-5" style="border: 22px solid white;">
-                    <i class="fa-solid fa-paintbrush mb-2" style="font-size: 30px;"></i>
-                    <h3 class="my-2">Design</h3>
-                    <p class="text_cut">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis dignissimos lorem Lorem ipsum... Kamal </p>
-                    <h6 class="my-3 more_box">
-                        <p class="More_text">More ↓</p>
-                        <p class="myText">laxman doloribus iste error, eligendi maiores itaque ab voluptas consectetur
-                            debitis </p>
-                    </h6>
+                <div class=" col-md-4" style="height: 350px;">
+                    <div class="col-md-12 text-center bg-black text-light p-5" style="border: 22px solid white;">
+                        <i class="fa-solid fa-paintbrush mb-2" style="font-size: 30px;"></i>
+                        <h3 class="my-2">Design</h3>
+                        <p class="text_cut">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis
+                            dignissimos lorem Lorem ipsum... Kamal </p>
+                        <h6 class="my-3 more_box">
+                            <p class="More_text">More ↓</p>
+                            <p class="myText">laxman doloribus iste error, eligendi maiores itaque ab voluptas
+                                consectetur
+                                debitis </p>
+                        </h6>
+                    </div>
                 </div>
-            </div>
 
-            <div class=" col-md-4" style="height: 350px;">
-                <div class="col-md-12 text-center bg-black text-light p-5" style="border: 22px solid white;">
-                    <i class="fa-solid fa-paintbrush mb-2" style="font-size: 30px;"></i>
-                    <h3 class="my-2">Design</h3>
-                    <p class="text_cut">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis dignissimos lorem Lorem ipsum... Kamal </p>
-                    <h6 class="my-3 more_box">
-                        <p class="More_text">More ↓</p>
-                        <p class="myText">laxman doloribus iste error, eligendi maiores itaque ab voluptas consectetur
-                            debitis </p>
-                    </h6>
+                <div class=" col-md-4" style="height: 350px;">
+                    <div class="col-md-12 text-center bg-black text-light p-5" style="border: 22px solid white;">
+                        <i class="fa-solid fa-paintbrush mb-2" style="font-size: 30px;"></i>
+                        <h3 class="my-2">Design</h3>
+                        <p class="text_cut">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis
+                            dignissimos lorem Lorem ipsum... Kamal </p>
+                        <h6 class="my-3 more_box">
+                            <p class="More_text">More ↓</p>
+                            <p class="myText">laxman doloribus iste error, eligendi maiores itaque ab voluptas
+                                consectetur
+                                debitis </p>
+                        </h6>
+                    </div>
                 </div>
+
+
             </div>
-
-
         </div>
     </div>
 
 
+    <!-- ====================================================Progress Work============================ -->
 
+    <!-- Progress Work -->
+    <div class="container-fluid progess_work_con py-5"
+        style="background: linear-gradient(to bottom, transparent, rgb(0, 0, 0));">
+        <div class="row">
+            <div class="col-md-12 progress_work_box text-center text-light">
+                <p class="pt-5" style="font-size: larger;">WORK PROCESS</p>
+            </div>
+            <div class="col-md-12 progress_work_box text-center text-light">
+                <p class="pb-4" style="font-size: xx-large; font-weight: bolder;">My Stages In A Project</p>
+            </div>
+
+            <div class="col-md-3 mb-4">
+                <div class="col-md-12 text-center bg-black text-light px-5 py-5">
+                    <i class="fa-solid fa-paintbrush mb-2" style="font-size: 30px;"></i>
+                    <h3 class="my-2">Design</h3>
+                    <p class="text_cut">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis dignissimos
+                        lorem Lorem ipsum... Kamal </p>
+                    <h6 class="my-3 more_box">
+                        <p class="More_text">More ↓</p>
+                        <p class="myText">laxman doloribus iste error, eligendi maiores itaque ab voluptas consectetur
+                            debitis </p>
+                    </h6>
+                </div>
+            </div>
+
+            <div class="col-md-3 mb-4">
+                <div class="col-md-12 text-center bg-black text-light px-5 py-5">
+                    <i class="fa-solid fa-paintbrush mb-2" style="font-size: 30px;"></i>
+                    <h3 class="my-2">Design</h3>
+                    <p class="text_cut">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis dignissimos
+                        lorem Lorem ipsum... Kamal </p>
+                    <h6 class="my-3 more_box">
+                        <p class="More_text">More ↓</p>
+                        <p class="myText">laxman doloribus iste error, eligendi maiores itaque ab voluptas consectetur
+                            debitis </p>
+                    </h6>
+                </div>
+            </div>
+
+            <div class="col-md-3 mb-4">
+                <div class="col-md-12 text-center bg-black text-light px-5 py-5">
+                    <i class="fa-solid fa-paintbrush mb-2" style="font-size: 30px;"></i>
+                    <h3 class="my-2">Design</h3>
+                    <p class="text_cut">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis dignissimos
+                        lorem Lorem ipsum... Kamal </p>
+                    <h6 class="my-3 more_box">
+                        <p class="More_text">More ↓</p>
+                        <p class="myText">laxman doloribus iste error, eligendi maiores itaque ab voluptas consectetur
+                            debitis </p>
+                    </h6>
+                </div>
+            </div>
+
+            <div class="col-md-3 mb-4">
+                <div class="col-md-12 text-center bg-black text-light px-5 py-5">
+                    <i class="fa-solid fa-paintbrush mb-2" style="font-size: 30px;"></i>
+                    <h3 class="my-2">Design</h3>
+                    <p class="text_cut">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis dignissimos
+                        lorem Lorem ipsum... Kamal </p>
+                    <h6 class="my-3 more_box">
+                        <p class="More_text">More ↓</p>
+                        <p class="myText">laxman doloribus iste error, eligendi maiores itaque ab voluptas consectetur
+                            debitis </p>
+                    </h6>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- ============================================================(SKILS)============================ -->
+    <!-- Skills -->
+    <div class="container-fluid bg-black skills_con">
+        <div class="row">
+            <div class="col-md-4 text-center text-light p-5 skills_box">
+                <h1 class="mb-4">My Skills</h1>
+                <p class="text-start">Vivamus sagittis felis non felis sagittis fringilla. Vivamus sagittis felis non
+                    elis sagittis fringilla. Vivamus sagittis felis non elis sagittis fringilla. Vivamus felis sagittis
+                    fringilla. In porttitor sem consequat, molestie leo ac, mollis mauris. Lorem, ipsum dolor sit amet
+                    consectetur adipisicing elit. Similique in, voluptatum, adipisci quod doloremque soluta officiis
+                    totam
+                    earum ipsum sunt voluptate? Suscipit aut ad magni vero facilis, accusantium aliquam omnis.</p>
+            </div>
+            <div class="col-md-4 text-light p-5 skills_box">
+                <h2 class="mb-4">Design</h2>
+                <p class="mb-0">Graphic Design</p>
+                <div class="progress mb-3 bg-dark" role="progressbar" aria-label="Info example" aria-valuenow="20"
+                    aria-valuemin="0" aria-valuemax="100">
+                    <div class="progress-bar bg-light text-dark" style="width: 20%">20%</div>
+                </div>
+                <p class="mb-0">Graphic Design</p>
+                <div class="progress mb-3 bg-dark" role="progressbar" aria-label="Info example" aria-valuenow="50"
+                    aria-valuemin="0" aria-valuemax="100">
+                    <div class="progress-bar bg-light text-dark" style="width: 50%">50%</div>
+                </div>
+                <p class="mb-0">Graphic Design</p>
+                <div class="progress mb-3 bg-dark" role="progressbar" aria-label="Info example" aria-valuenow="75"
+                    aria-valuemin="0" aria-valuemax="100">
+                    <div class="progress-bar bg-light text-dark" style="width: 75%">75%</div>
+                </div>
+                <p class="mb-0">Graphic Design</p>
+                <div class="progress mb-3 bg-dark" role="progressbar" aria-label="Info example" aria-valuenow="100"
+                    aria-valuemin="0" aria-valuemax="100">
+                    <div class="progress-bar bg-light text-dark" style="width: 100%">100%</div>
+                </div>
+            </div>
+            <div class="col-md-4 text-light p-5 skills_box">
+                <h2 class="mb-4">Development</h2>
+                <p class="mb-0">Web Development</p>
+                <div class="progress mb-3 bg-dark" role="progressbar" aria-label="Info example" aria-valuenow="40"
+                    aria-valuemin="0" aria-valuemax="100">
+                    <div class="progress-bar bg-light text-dark" style="width: 40%">40%</div>
+                </div>
+                <p class="mb-0">Web Development</p>
+                <div class="progress mb-3 bg-dark" role="progressbar" aria-label="Info example" aria-valuenow="60"
+                    aria-valuemin="0" aria-valuemax="100">
+                    <div class="progress-bar bg-light text-dark" style="width: 60%">60%</div>
+                </div>
+                <p class="mb-0">Web Development</p>
+                <div class="progress mb-3 bg-dark" role="progressbar" aria-label="Info example" aria-valuenow="80"
+                    aria-valuemin="0" aria-valuemax="100">
+                    <div class="progress-bar bg-light text-dark" style="width: 80%">80%</div>
+                </div>
+                <p class="mb-0">Web Development</p>
+                <div class="progress mb-3 bg-dark" role="progressbar" aria-label="Info example" aria-valuenow="95"
+                    aria-valuemin="0" aria-valuemax="100">
+                    <div class="progress-bar bg-light text-dark" style="width: 95%">95%</div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
@@ -183,7 +346,7 @@
 
 <script>
     var paragraphs = document.querySelectorAll('.text_cut');
-    paragraphs.forEach(function(paragraph) {
+    paragraphs.forEach(function (paragraph) {
         var text = paragraph.textContent;
         var truncatedText = text.substring(0, 100);
         paragraph.textContent = truncatedText;
